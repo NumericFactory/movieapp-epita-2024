65b383ae00b5e94fe27c968f0c6ecb21a262d8a3
NumericFactory <frederic.lossignol@gmail.com>
2024-01-18 13:01:26 +0100

    explication de Observable.pipe()

--- src/app/shared/services/movie.service.ts
+++ src/app/shared/services/movie.service.ts
@@ -14,12 +14,20 @@ export class MovieService {
 
   constructor(private http: HttpClient) { }
 
+  /*
+    Observable.pipe() return un Observable
+
+    .pipe( ) accepte en parametres des opérateurs de transformation : 
+    map(), filter(), etc....
+
+    On utilise pipe() pour donner à nos components un Observable qui contient
+    des données formatées comme on le décide
+
+    Ainsi on délégue totalement au service la responsabilité de mapper les
+    reponses API en modeles d'objets côté front-end
+  */
   getMoviesFromApi(): Observable<MovieModel[]> {
     const ENDPOINT = `/discover/movie`;
-    // const HEADERS = new HttpHeaders({
-    //   Authorization: 'Bearer ' + this.API_TOKEN,
-    //   accept: 'application/json'
-    // });
     let options = {
       headers: {
         Authorization: 'Bearer ' + this.API_TOKEN,
@@ -27,10 +35,12 @@ export class MovieService {
       },
       params: { language: 'fr' }
     }
-    // this.http.get(url, {headers: value, params:value})
     return this.http.get(this.TMDB_URL + ENDPOINT, options).pipe(
-      map((response: any) => response.results.map((movieFromApi: any) => new MovieModel(movieFromApi))),
+      map((response: any) =>
+        response.results.map(
+          (movieFromApi: any) => new MovieModel(movieFromApi)
+        )
+      ),
     )
-
   }
 }
