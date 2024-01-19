c85c258561b6c709ca6fe51ee04d3355093d4c40
NumericFactory <frederic.lossignol@gmail.com>
2024-01-19 18:03:47 +0100

    utiliser la class ActivatedRoute pour recuperer un param Url (dans MovieDetailViewComponent)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovieListViewComponent } from './views/movie-list-view/movie-list-view.component';
 import { TvListViewComponent } from './views/tv-list-view/tv-list-view.component';
+import { MovieDetailViewComponent } from './views/movie-detail-view/movie-detail-view.component';
 
 const routes: Routes = [
   // ajouter mes routes
   { path: '', component: MovieListViewComponent },
-  { path: 'tv', component: TvListViewComponent }
+  { path: 'tv', component: TvListViewComponent },
+  { path: 'movie-detail/:id', component: MovieDetailViewComponent }
 
 ];
 
--- src/app/views/movie-detail-view/movie-detail-view.component.ts
+++ src/app/views/movie-detail-view/movie-detail-view.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieModel } from '../../shared/models/movie.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-detail-view',
   templateUrl: './movie-detail-view.component.html',
   styleUrl: './movie-detail-view.component.scss'
 })
-export class MovieDetailViewComponent {
+export class MovieDetailViewComponent implements OnInit {
+
+  movie!: MovieModel;
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  // 1 récupere l'id dans l'URL
+  ngOnInit() {
+    console.log(this.route.snapshot.params)
+  }
+
+  // 2 demander au service : faire une request TMDB_URL/movie/{id} 
+  //movieService.getDetailMovie(id)
+  // Je recupere un objet MovieModel
+
+  // 3 dans le template movie-detail-.html
 
 }
