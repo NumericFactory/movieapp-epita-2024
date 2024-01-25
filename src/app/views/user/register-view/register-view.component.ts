3a73a4e8cc08f199a61aded0ed122c5a15af022b
NumericFactory <frederic.lossignol@gmail.com>
2024-01-25 16:50:51 +0100

    creation du registerForm + validation

--- src/app/views/user/register-view/register-view.component.ts
+++ src/app/views/user/register-view/register-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-view',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrl: './register-view.component.scss'
 })
 export class RegisterViewComponent {
+
+  userForm!: FormGroup;
+  isSubmitted: boolean = false;
+
+  constructor(private fb: FormBuilder) { }
+
+
+  ngOnInit() {
+    /**
+     * Créer une instance de FormGroup
+     */
+    this.userForm = this.fb.group({
+      username: ['',
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(10)
+      ],
+      password: ['',
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(5)
+      ]
+    })
+  }
+
+  /**
+   * Role verifier la validité et appeler 
+   * la méthode createUser du UserService
+   */
+  onSubmitUserRegisterForm(): void {
+    this.isSubmitted = true;
+    console.log(this.userForm.value);
+    console.log('is Valid ? ', this.userForm.valid)
+  }
 
 }
