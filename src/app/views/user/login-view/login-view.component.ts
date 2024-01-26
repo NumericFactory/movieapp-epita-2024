1bab61bca96eafdc91f2a7efab856b47a0d69743
NumericFactory <frederic.lossignol@gmail.com>
2024-01-26 01:21:23 +0100

    Ajout du formulaire de connexion

--- src/app/views/user/login-view/login-view.component.ts
+++ src/app/views/user/login-view/login-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../../shared/services/user.service';
 
 @Component({
   selector: 'app-login-view',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrl: './login-view.component.scss'
 })
 export class LoginViewComponent {
+  userForm!: FormGroup;
+  isSubmitted: boolean = false;
 
+  constructor(private fb: FormBuilder, private userSvc: UserService) { }
+
+
+  ngOnInit() {
+    /**
+     * Créer une instance de FormGroup
+     */
+    this.userForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(10)
+      ]
+      ],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(5)
+      ]
+      ]
+    })
+  }
+
+  /**
+   * Role verifier la validité et appeler 
+   * la méthode createUser du UserService
+   */
+  onSubmitUserRegisterForm(ev: Event): void {
+    ev.preventDefault()
+
+    this.isSubmitted = true;
+    if (this.userForm.valid) {
+      this.isSubmitted = false
+      /* Remettre à zero form*/
+      // this.userForm.reset();
+      // createUser
+      console.log(this.userForm.value)
+      this.userSvc.createUser(this.userForm.value)
+        .subscribe(data => console.log(data))
+
+    }
+  }
 }
