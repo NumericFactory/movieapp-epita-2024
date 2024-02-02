abf6da65b6e5cb4719134d224e324ce7ab1b7a4b
NumericFactory <frederic.lossignol@gmail.com>
2024-02-02 10:49:35 +0100

    refactor token.interceptor.ts

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -12,54 +12,53 @@ interface Endpoint {
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
+  // list of endpoints needs a user token
+  private apiAuthEndpoints: Endpoint[] = [
+    { endpoint: '/watchlist', method: 'ALL' },
+    { endpoint: 'reviews', method: 'POST' },
+    // add other...
+  ];
   TMDB_URL = environment.TMDB_API_URL;
   TMDB_TOKEN = environment.TMDB_TOKEN;
   MYAPI = environment.API_BASE_URL;
   USER_TOKEN = this.userSvc.getToken();
 
-  /* endpoints needs a user token in our API */
-  apiAuthEndpoints: Endpoint[] = [
-    { endpoint: '/watchlist', method: 'ALL' },
-    { endpoint: 'reviews', method: 'POST' },
-    // add other endpoints...
-  ];
-
   constructor(private userSvc: UserService) { }
 
   /** 
-   * Ici le rôle de intercept est d'intercepter req (HttpRequest Object)
-   * afin dajouter un nom un token selon l'API et le endpoint appelé
-   * 
-   * > Si on appelle l'API TMDB 
-   *   on set les headers {Authorization : ' bearer [tokenTMDB]'}
-   * 
-   * > Si on appelle notre API 
-   *   selon l'URL et method, on set les headers {Authorization : ' bearer [tokenUtilisateur]'}
+   * le rôle de intercept est d'intercepter req (HttpRequest Object)
+   * afin dajouter un nom un token (on the fly) selon :
+   * l'API appelée, le endpoint et la méthode 
   */
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     console.log('token interceptor', req);
     let cloneRequest: HttpRequest<any> = req;
+
     // SI API TMDB
     if (req.url.includes(this.TMDB_URL)) {
       cloneRequest = this.addBearerToken(req, this.TMDB_TOKEN);
     }
+
     // SI notre API et SI l'url necessite l'authentification
     if (this.isUrlNeedsUserToken(req)) {
       if (this.USER_TOKEN !== null)
         cloneRequest = this.addBearerToken(req, this.USER_TOKEN);
     }
+
     return next.handle(cloneRequest);
   }
 
 
-  /*  
-    Check if : 
+
+
+  /**
+    * Check if 
     * url called is my api 
     * url needs user token or not
     * @param request:HttpRequest
     * @returns boolean
   */
-  isUrlNeedsUserToken(request: HttpRequest<any>): boolean {
+  private isUrlNeedsUserToken(request: HttpRequest<any>): boolean {
     if (request.url.includes(this.MYAPI)) {
       let endpoint = Utils.getUrlEndpoint(request.url, this.MYAPI);
       // verify if the request is in apiAuthEndpoints Array we defined
@@ -81,7 +80,7 @@ export class TokenInterceptor implements HttpInterceptor {
    * @param token 
    * @returns req:HttpRequest
    */
-  addBearerToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
+  private addBearerToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
     return request.clone({
       headers: request.headers
         .append('Authorization', `Bearer ${token}`)
